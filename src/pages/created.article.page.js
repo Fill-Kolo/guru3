9b359a9a78700fc5bf6f939e3fe501aa8050f9d8
Колотадзе Филипп <f.kolotadze.ext@skillaz.ru>
2025-05-29 21:51:23 +0300

    lesson5ver4

---
+++ src/pages/created.article.page.js
@@ -0,0 +1,38 @@
+﻿export class CreatedArticle {
+	locators = {
+		//данный локатор ищет поле с placeholder "Article Title"
+		articleName: '[placeholder="Article Title"]',
+		//данный локатор ищет поле с placeholder "What's this article about?"
+		articleAbout: '[placeholder="What\'s this article about?"]',
+		//данный локатор ищет поле с placeholder "Write your article (in markdown)"
+		articleWrite: '[placeholder="Write your article (in markdown)"]',
+		//данный локатор ищет кнопку Publish Article, которая отвечает за публикацию статьи
+		buttonPublishArticle: 'button.btn.btn-lg.pull-xs-right.btn-primary'
+	}
+
+
+	constructor(page) {
+		this.page = page;
+		this.buttonNewArticle = page.getByRole('link', { name: 'New Article' });
+
+	}
+
+	//clickaem na knopky New Article
+	async clickNewArticle() {
+		await this.buttonNewArticle.click();
+	}
+
+	//кликаем на поля и заполняем их значения
+	async writeArticle(randomText) {
+		const { articleName, articleAbout, articleWrite } = randomText;
+
+		await this.page.locator(this.locators.articleName).click()
+		await this.page.locator(this.locators.articleName).fill(articleName);
+		await this.page.locator(this.locators.articleAbout).click();
+		await this.page.locator(this.locators.articleAbout).fill(articleAbout);
+		await this.page.locator(this.locators.articleWrite).click();
+		await this.page.locator(this.locators.articleWrite).fill(articleWrite);
+		await this.page.locator(this.locators.buttonPublishArticle).click();
+	}
+
+}
---
+++ tests/lesson5V2.spec.js
@@ -0,0 +1,78 @@
+﻿import { test, expect } from '@playwright/test';
+import { MainPage } from '../src/pages/main.page';
+import { RegisterPage } from '../src/pages/register.page';
+import { UserBuilder } from '../src/helpers/buillders/user.builder';
+import { YourFeedPage } from '../src/pages/yourfeed.page';
+//ниже импорт того, что я создавал в НОВЫХ pages
+import { CreatedArticle } from '../src/pages/created.article.page'
+import { RandomTextBuilder } from '../src/helpers/buillders/random.text.builder'
+import { WriteComment } from '../src/pages/write.comment.page'
+import { TextFirstCommentArticlePage } from '../src/pages/text.comment.article.page'
+
+
+
+test('Создаем новую статью', async ({
+	page,
+}) => {
+	// Gotovim stranichki
+
+	const mainpage = new MainPage(page);
+	const registerPage = new RegisterPage(page);
+	const yourFeedPage = new YourFeedPage(page);
+	const createdArticle = new CreatedArticle(page)
+
+	//Генерируем рандомного юзера
+	const randomUser = new UserBuilder()
+		.addEmail()
+		.addPassword(14)
+		.addUsername()
+		.generate();
+
+	//Генерируем текст нашей статья
+	const randomTextBuilder = new RandomTextBuilder()
+		.addArticleName(4)
+		.addArticleAbout(2)
+		.addArticleWrite(1)
+		.generate();
+
+	await mainpage.open();
+	await mainpage.gotoLogin();
+	await registerPage.signup(randomUser);
+	await createdArticle.clickNewArticle();
+	await createdArticle.writeArticle(randomTextBuilder);
+	await expect(yourFeedPage.profileNameField).toContainText(randomUser.username);
+});
+
+
+test('Оставляем комментарий под первой стаьей', async ({
+	page,
+}) => {
+	// Gotovim stranichki
+
+	const mainpage = new MainPage(page);
+	const registerPage = new RegisterPage(page);
+	const writeComment = new WriteComment(page);
+	const textFirstCommentArticlePage = new TextFirstCommentArticlePage(page);
+
+	//Генерируем рандомного юзера
+	const randomUser = new UserBuilder()
+		.addEmail()
+		.addPassword(10)
+		.addUsername()
+		.generate();
+
+	//Генерируем текст комментарий к статье
+	const randomTextBuilder = new RandomTextBuilder()
+		.addArticleWrite(1)
+		.generate();
+
+	await mainpage.open();
+	await mainpage.gotoLogin();
+	await registerPage.signup(randomUser);
+	await writeComment.clickGlobalFeed();
+	await writeComment.clickFirstArticle();
+	await writeComment.clickAndWriteCommen(randomTextBuilder);
+	
+	await expect(textFirstCommentArticlePage.textLastCommentOnArticle).toContainText(randomTextBuilder.articleWrite);
+
+});
